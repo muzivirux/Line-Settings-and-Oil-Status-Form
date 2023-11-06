360a0a9b9e573a3c2e88b3847f1d1ed1f7620bc1
Muzaffar Rafiq <rare.angel52@gmail.com>
2023-11-06 21:48:51 +0500

    auto added time bsaed on the shift code

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-// auto shift time add based on the shift code
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the HTML elements
+  const shiftSelect = document.getElementById("Shift");
+  const shiftTimeInput = document.getElementById("shiftTime");
 
+  // Add an event listener to the select element
+  shiftSelect.addEventListener("change", function () {
+    // Get the selected option value
+    const selectedValue = shiftSelect.value;
+
+    // Determine the time based on the selected option
+    let time = "00:00"; // Default value
+    if (selectedValue === "M") {
+      time = "9:00 AM"; // Morning time
+    } else if (selectedValue === "E") {
+      time = "9:00 PM"; // Evening time
+    }
+
+    // Update the time input field
+    shiftTimeInput.value = time;
+  });
+});
