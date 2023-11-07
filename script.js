b444290311a55dbb64421905fa15f04768cf64f8
muzivirux <91391588+muzivirux@users.noreply.github.com>
2023-11-07 14:45:39 +0500

    remove comment

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get references to the HTML elements
+  
   const shiftSelect = document.getElementById("Shift");
   const shiftTimeInput = document.getElementById("shiftTime");
 
-  // Add an event listener to the select element
+  
   shiftSelect.addEventListener("change", function () {
-    // Get the selected option value
+   
     const selectedValue = shiftSelect.value;
 
-    // Determine the time based on the selected option
-    let time = "00:00"; // Default value
+    
+    let time = "00:00"; 
     if (selectedValue === "M") {
-      time = "9:00 AM"; // Morning time
+      time = "9:00 AM"; 
     } else if (selectedValue === "E") {
-      time = "9:00 PM"; // Evening time
+      time = "9:00 PM"; 
     }
-
-    // Update the time input field
-    shiftTimeInput.value = time;
+   shiftTimeInput.value = time;
   });
 });
