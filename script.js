14ba630415124cb6b795cf8e42577126efb63eea
Muzaffar Rafiq <rare.angel52@gmail.com>
2023-11-17 15:05:53 +0500

    update shift timing from 7 to 7

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
   const shiftSelect = document.getElementById("Shift");
   const shiftTimeInput = document.getElementById("shiftTime");
 
-  
   shiftSelect.addEventListener("change", function () {
-   
     const selectedValue = shiftSelect.value;
 
-    
-    let time = "00:00"; 
+    let time = "00:00";
     if (selectedValue === "M") {
-      time = "9:00 AM"; 
+      time = "7:00 AM";
     } else if (selectedValue === "E") {
-      time = "9:00 PM"; 
+      time = "7:00 PM";
     }
-   shiftTimeInput.value = time;
+    shiftTimeInput.value = time;
   });
 });
